72e206ad55e74d705614ea7b2bec71ce7d3b20dd
ThiroSmash <teodoro_11@hotmail.es>
2021-03-05 18:03:10 +0100

    added marker ID as input parameter

--- MarkerCreator.py
+++ MarkerCreator.py
@@ -1,13 +1,24 @@
 import numpy as np
 import cv2, time
 import cv2.aruco as aruco
+
+
+
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--id", type=int, default=0,
+	help="ID of the marker to be written")
+
+args = vars(ap.parse_args())
+
 aruco_dict = aruco.Dictionary_get(aruco.DICT_6X6_250)
-print(aruco_dict)
+
 # second parameter is id number
 # last parameter is total image size
-img = aruco.drawMarker(aruco_dict, 0, 240)
-cv2.imwrite("test_marker.jpg", img)
+img = aruco.drawMarker(aruco_dict, args["id"], 240)
+name = "marker" + str(args["id"]) + ".jpg"
 
+cv2.imwrite(name, img)
 cv2.imshow('frame',img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
